35b53830439962e79d87e1c0dba0f724760b721f
Aleksa's PC <asiljic59@gmail.com>
2025-12-07 14:12:00 +0100

    Clean proj files and apply gitignore

--- FastFood_SV_50_2022/Main.cpp
+++ FastFood_SV_50_2022/Main.cpp
@@ -15,7 +15,7 @@
 #include "ColorMesh.h"
 
 
-const int ORDER_COUNT = 9;
+const int ORDER_COUNT = 10;
 
 //za dugme na pocetku
 struct Button {
@@ -25,7 +25,14 @@ struct Button {
 	float w, h;     // width and height
 	bool clicked = false;
 };
-
+/// <summary>
+/// Stavke koje se stavljaju na burger
+/// texture -> tekstura obuhvacena stavkom
+/// mesh -> VAO + VBO
+/// x -> koordinata koja se pamti
+/// y -> koordinata koja se pamti
+/// cooked -> sluzi samo za patty
+/// </summary>
 struct BurgerPart {
 	Texture* texture;
 	Mesh* mesh;
@@ -48,7 +55,7 @@ bool IsInsideButton(Button b, float mx, float my)
 		my >(b.y - b.h) &&
 		my < (b.y + b.h);
 }
-
+//da li je burger na sporetu??
 bool PattyOverStove()
 {
 	float stoveMinX = -1.0f;
@@ -56,10 +63,10 @@ bool PattyOverStove()
 	float stoveMinY = -1.0f;
 	float stoveMaxY = 0.0f;
 
-	// patty final drawn size in NDC
 	float pattyWidthHalf = 0.1f; // 0.2 total width
 	float pattyHeightHalf = 0.1f; // 0.2 total height
 
+	//uzimamo koordinate patty tako sto x,y umanjimo/povercamo sa poloviom duzine/visine i dobijemo granice
 	float pattyMinX = uX - pattyWidthHalf;
 	float pattyMaxX = uX + pattyWidthHalf;
 	float pattyMinY = uY - pattyHeightHalf;
@@ -70,20 +77,15 @@ bool PattyOverStove()
 
 	return overlapX && overlapY;
 }
-bool ItemOverArea(float itemMinX, float itemMaxX,
-	float prevMinX, float prevMaxX)
-{
-	// true ako postoji makar malo preklapanje
-	bool overlapX = !(itemMaxX < prevMinX || itemMinX > prevMaxX);
-	return overlapX;
-};
 
+
+//funkcija za dobvijanje polovine sirine
 float GetHalfWidth(const Mesh* m)
 {
 	float minX = 9999;
 	float maxX = -9999;
 
-	// XY XY XY XY (we read only X coordinates)
+	// XY XY XY XY
 	for (int i = 0; i < 8; i += 2)
 	{
 		float x = m->verticesCopy[i];
@@ -94,12 +96,13 @@ float GetHalfWidth(const Mesh* m)
 	return (maxX - minX) / 2.0f;
 }
 
+//funkcija za dobijanje pola visine
 float GetHalfHeight(const Mesh* m)
 {
 	float minY = 9999;
 	float maxY = -9999;
 
-	// XY XY XY XY (read only Y coordinates)
+	// XY XY XY XY
 	for (int i = 1; i < 8; i += 2)
 	{
 		float y = m->verticesCopy[i];
@@ -116,21 +119,27 @@ float GetHalfHeight(const Mesh* m)
 float uX = 0.0f;
 float uY = 0.0f;
 
+//pratimo progres kuhanja
 float cookingProgress = 0.0f;
 
+
+//porudzbina pocela, burger pecen
 bool orderStarted = false;
 bool pattyCooked = false;
+
+//kao neka vrsta cooldown metode jer kad se pritisne za ketcuhp on odmah doda i za senf
 bool spacePressed = false;
 bool spaceWasDown = false;
 
-
-
+//delovi burgera
 const int MAX_PARTS = 10;
 
+//vec postavljeni delovi burgera koji ima maksimalno koliko i delova
 BurgerPart placed[MAX_PARTS];
 int placedCount = 0;
 int currentItem = 0;
 
+//za ketchup i senf imamo i spill gde se pamti i iscrtava svaki put kad ne pritisnemo space iznad burgera
 Spill spills[50];
 int spillCount = 0;
 
@@ -188,10 +197,20 @@ int main()
 	glViewport(0, 0, mode->width, mode->height);
 
 
-
+	/// <summary>
+	/// Ucitavamo sejdere kojih imamo 3
+	/// shaderProgram -> za sve teksture koriscen
+	/// colorShader -> za progress bar koji se jedini iscrtava
+	/// pattyShader -> za burger koji se boji dok je na sporetu
+	/// </summary>
 	Shader shaderProgram("default.vert", "default.frag");
 	Shader colorShader("color.vert", "color.frag");
 	Shader pattyShader("patty.vert", "patty.frag");
+
+
+	/// <summary>
+	///Prvo se iscrtava START dugme
+	/// </summary>
 
 	float buttonVertices[] = {
 		// X,Y        U,V
@@ -209,6 +228,10 @@ int main()
 
 	Mesh buttonMesh(buttonVertices, sizeof(buttonVertices));
 
+	/// <summary>
+	/// Ime stoji sve vreme
+	/// </summary>
+
 	float nameVertices[] = {
 		// X      Y      U     V
 		0.8f,  0.8f,    0.0f,  0.0f,  // bottom-left
@@ -217,6 +240,12 @@ int main()
 		1.0f,  1.0f,    1.0f,  1.0f   // top-right
 	};
 	Mesh nameMesh(nameVertices, sizeof(nameVertices));
+
+
+	/// <summary>
+	/// PRVI DEO PROGRAMA GDE SE ISCRTAVAJU SPORET I PLJEKSAVICA,
+	/// KAO I PROGRESS BAR DOK JE PLJEKSAVICA NA SPORETU
+	/// </summary>
 
 	float stoveVertices[] = {
 		//   X       Y       U     V
@@ -253,6 +282,12 @@ int main()
 	Texture smederevac("smederevac.png", GL_TEXTURE2);
 	Texture patty("patty.png", GL_TEXTURE3);
 
+	/// <summary>
+	/// DRUGI DEO PROGRAMA
+	/// ISCRTAVA SE STO I TANJIR
+	/// </summary>
+
+
 	//patty cooked textures
 	float tableVertices[] = {
 		// X      Y      U   V
@@ -276,6 +311,11 @@ int main()
 	Mesh plateMesh = Mesh::Mesh(plateVertices, sizeof(plateVertices));
 
 
+	/// <summary>
+	/// Za svaki element burgera kreiramo MESH -> VAO+VBO struktura
+	/// Ucitravamo i teksture za sve elemente
+	/// </summary>
+
 	//bottom bun
 	float bottomBunVertices[] = {
 		-0.1f, -0.1f, 0,0,
@@ -291,16 +331,6 @@ int main()
 		 0.4f,  0.8f, 1,1
 	};
 	Mesh bottomBunMesh(bottomBunVertices, sizeof(bottomBunVertices));
-	Mesh ketchupBottleMesh = Mesh::Mesh(rectVertices, sizeof(rectVertices));
-	Mesh mustardBottleMesh = Mesh::Mesh(rectVertices, sizeof(rectVertices));
-	Mesh slicedPicklesMesh = Mesh::Mesh(rectVertices, sizeof(rectVertices));
-	Mesh onionMesh = Mesh::Mesh(rectVertices, sizeof(rectVertices));
-	Mesh lettuceMesh = Mesh::Mesh(rectVertices, sizeof(rectVertices));
-	Mesh cheseeSliceMesh = Mesh::Mesh(rectVertices, sizeof(rectVertices));
-	Mesh topBunMesh = Mesh::Mesh(rectVertices, sizeof(rectVertices));
-	Mesh ketchupSplashMesh = Mesh::Mesh(rectVertices, sizeof(rectVertices));
-	Mesh mustardSplashMesh = Mesh::Mesh(rectVertices, sizeof(rectVertices));
-
 	Mesh greetingsMesh(greetingsVertices, sizeof(greetingsVertices));
 
 
@@ -320,7 +350,13 @@ int main()
 	Texture mustardSplash("mustard_splash.png", GL_TEXTURE13);
 	Texture ketchupLayer("ketchup_layer.png", GL_TEXTURE14);
 	Texture mustardLayer("mustard_layer.png", GL_TEXTURE15);
+	Texture tomato("tomato.png", GL_TEXTURE15);
 	Texture greetings("greetings.png", GL_TEXTURE3);
+
+
+	/// <summary>
+	/// Sluzi kako bi po redu redjali nase elemente na burger
+	/// </summary>
 
 
 	BurgerPart order[] = {
@@ -332,11 +368,12 @@ int main()
 		{ &onion,  &rectMesh },
 		{ &lettuce,  &rectMesh },
 		{ &cheese,  &rectMesh },
+		{ &tomato,  &rectMesh },
 		{ &topBun,  &rectMesh },
 	};
 
 		
-
+	// zuta boja pozadine
 	glClearColor(0.95f, 0.80f, 0.30f, 1.0f);
 
 	// Cistimo bafer da bi stavili novu boju
@@ -354,7 +391,7 @@ int main()
 	{
 		glfwPollEvents();
 
-
+		//listener za WASD (kretanje)
 		float speed = 0.01f;
 
 		if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) uY += speed;
@@ -369,38 +406,44 @@ int main()
 
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		//Ime je uvek tu bez obzira na stanje programa
 		shaderProgram.Activate();
 		name.Draw(shaderProgram,nameMesh.vao,0.0f,0.0f,1.0f);
 
-		if (PattyOverStove())
-		{
-			cookingProgress -= 0.005f;  // or heat transfer
-		}
-		else
-		{
-			cookingProgress += 0.005f; // cool down
-		}
-
-
-		if (cookingProgress < 0.0f) cookingProgress = 0.0f;
-		if (cookingProgress >= 2.0f && !pattyCooked) {
-			cookingProgress = 2.0f;
-			pattyCooked = true;
-			orderStarted = false;
-
-			placedCount = 0;
-			currentItem = 0;
-
-			uX = 0.0f;
-			uY = 0.9f;
-		}
+	
 
 
 		if (orderStarted)
 		{
+
+			//da li je burger preko sporeta???
+			if (PattyOverStove())
+			{
+				cookingProgress -= 0.005f;  // or heat transfer
+			}
+			else
+			{
+				cookingProgress += 0.005f; // cool down
+			}
+
+			// za kontrolisemo da cooking progress ide od 0 do 2 (2 je zato sto tehnicki ide po X osi od -1 do 1
+			if (cookingProgress < 0.0f) cookingProgress = 0.0f;
+			if (cookingProgress >= 2.0f && !pattyCooked) {
+				cookingProgress = 2.0f;
+				pattyCooked = true;
+				orderStarted = false;
+
+				placedCount = 0;
+				currentItem = 0;
+
+				uX = 0.0f;
+				uY = 0.9f;
+			}
+
 			// texturing shader active
 			shaderProgram.Activate();
-
+			
+			//iscrtavamo smederevac i burger koji bojimo toliko koliko se ispekao (cooking progress napredovao)
 			smederevac.Draw(shaderProgram, stoveMesh.vao, 0.0f, 0.0f, 1.0f);
 
 			pattyShader.Activate();
@@ -410,37 +453,46 @@ int main()
 			float leftX = -1.0f;
 			float rightX = leftX + (cookingProgress * 1.0f); // moves from -1 → 0
 
+			//povecavamo gornji i donji desni X koordinatu
 			progressBarVertices[2 * 6 + 0] = rightX;   // top-right X
 			progressBarVertices[3 * 6 + 0] = rightX;   // bottom-right X
 
-			// Upload to GPU
+			// Azuriranje progress bara zato sto se povecava cooking progress
 			progressBar.UpdateVertices(progressBarVertices);
 
 			colorShader.Activate();
 			progressBar.Draw(colorShader,
 				0.0f,
 				0.0f,
-				1.0f,    // IMPORTANT: scale stays 1!
+				1.0f,   
 				1.0f
 			);
 
 	
 
 		}
+
+
+		/// <summary>
+		/// DRUGI DEO (DAL LI JE ISPECEN BURGER PROVERA)
+		/// </summary>
+
 		else if (pattyCooked)
 		{
+			//bezuslovno se crta sto i tanjir
 			shaderProgram.Activate();
 			table.Draw(shaderProgram, tableMesh.vao, 0, 0, 1);
 			plate.Draw(shaderProgram, plateMesh.vao, 0, 0, 1);
 
+			//tekstura i MESH odgovarajuceg trenutnog itema koji se crta
 			Texture* texture;
 			Mesh* mesh;
 
 			// nacrtaj već postavljene delove
 			for (int i = 0; i < placedCount; i++)
 			{
+				// if else zato sto moze da bude patty (tu koristimo drugi sejder) a moze biti i bilo koji drugi element
 				if (placed[i].texture == &patty) {
-					// draw with shader that uses cooked uniform
 					pattyShader.Activate();
 					glUniform1f(
 						glGetUniformLocation(pattyShader.ID, "cook"),
@@ -452,6 +504,7 @@ int main()
 						placed[i].y,
 						1.5f);
 				}
+				//ako je bilo koji drugi element onda se koristi basic sejder za teksture
 				else {
 					shaderProgram.Activate();
 					placed[i].texture->Draw(shaderProgram,
@@ -461,21 +514,24 @@ int main()
 						1.5f);
 				}
 			}
+			//crtamo sve neuspesne pokusaje stavljanja kecapa i senfa
+			// oni ostaju tu do kraja programa
+			for (int s = 0; s < spillCount; s++)
+			{
+				shaderProgram.Activate();
+				spills[s].texture->Draw(shaderProgram,
+					rectMesh.vao,
+					spills[s].x,
+					spills[s].y,
+					1.5f);
+			}
 
 
 			// ako još imamo sastojke u porudžbini
+			//dok imamo sastojaka vrtece se petlja
 			if (currentItem < ORDER_COUNT)
 			{
-				for (int s = 0; s < spillCount; s++)
-				{
-	
-					spills[s].texture->Draw(shaderProgram,
-						rectMesh.vao,
-						spills[s].x,
-						spills[s].y,
-						1.5f);
-				}
-
+				//ako je trenutni item patty onda ga crtamo pecenog 
 				if (currentItem == 1) {
 					pattyShader.Activate();
 					glUniform1f(
@@ -491,6 +547,7 @@ int main()
 						1.5f
 					);
 				}
+				//u suprotnom normalno crtanje
 				else {
 					shaderProgram.Activate();
 					order[currentItem].texture->Draw(
@@ -503,10 +560,13 @@ int main()
 				}
 
 
-
+				// inicijalno stavljamo teksturu i mesh da budu od trenutne stavke
+				//ovo nije uvek isto i videcemo zasto (DOLE PRIMER ZA KECAP SENF)
 				Texture* texture = order[currentItem].texture;
 				Mesh* mesh = order[currentItem].mesh;
 
+
+				//racunamo ako je kecap/senf u pitanju
 				float halfW = GetHalfWidth(order[currentItem].mesh) * 1.5f;
 				float halfH = GetHalfHeight(order[currentItem].mesh) * 1.5f;
 
@@ -515,12 +575,20 @@ int main()
 				float newY = uY;
 				bool spaceNow = glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS;
 
-				if (spaceNow && !spaceWasDown)  // <-- triggers ONLY once per press
+				//ovo je bilo potrebno jer inicijalno se desavalo da kecap i senf odjednom dodaju na burger
+				//a klik se desio samo jednom
+				//tako da bi se detektovalno JEDNOM po kliku
+				if (spaceNow && !spaceWasDown)  // <-- triggeruje se samo za jedan KLIK
 				{
 					if (placedCount == 2 || placedCount == 3)
 					{
-						Texture* splash = (placedCount == 2) ? &ketchupLayer : &mustardLayer;
+						//fini layer kecapa i senfa koji ide na burger ako je boca iznad njega
+						Texture* layer = (placedCount == 2) ? &ketchupLayer : &mustardLayer;
+						
+						//splash koji ide na sto usled neuspesnog pokusaja
+						Texture* splash = (placedCount == 2) ? &ketchupSplash: &mustardSplash;
 
+						//prosli deo
 						BurgerPart prev = placed[placedCount - 1];
 
 						float prevHalfW = GetHalfWidth(prev.mesh) * 1.5f;
@@ -532,6 +600,7 @@ int main()
 						float prevMinX = prev.x - prevHalfW;
 						float prevMaxX = prev.x + prevHalfW;
 
+						//racunamo da li je iznad proslog dela burgera(burgera in general)
 						bool aboveBurger = !(currMaxX < prevMinX || currMinX > prevMaxX);
 
 						if (aboveBurger)
@@ -540,9 +609,10 @@ int main()
 							newX = prev.x;
 							newY = prev.y + 0.03f;
 
-							texture = splash;
+							texture = layer;
 							mesh = &rectMesh;
 						}
+						//ako nije iznad dodajemo u spills
 						else
 						{
 							spills[spillCount] = { splash, uX, -1.8f };
@@ -554,7 +624,9 @@ int main()
 				spaceWasDown = spaceNow; // update for next frame
 
 
-
+				//malo hard kodovano za prvi element
+				//ali u ovom slucaju je tanjir staticki element
+				//pa nije strasno uzeti njegovu koordinatu hard coded
 				if (placedCount == 0)
 				{
 					bool xOverlap =
@@ -573,6 +645,8 @@ int main()
 						std::cout << "Bun Y" << newY << std::endl;
 					}
 				}
+
+				//ako su delovi kecap boca i senf boca preskace se ovaj deo
 				else if(placedCount != 2 && placedCount != 3)
 				{
 					BurgerPart prev = placed[placedCount - 1];
@@ -580,48 +654,57 @@ int main()
 					// DO NOT use half heights anymore
 					float snapY = prev.y + 0.03f;
 
-					// Only require that patty passes close vertically
+					// Emprijiski zakljuceno da snapY mora se umanjiti 1.3f kod burgera
 					bool yCloseEnough;
 					if (placedCount == 1) {
 						yCloseEnough = (uY <= snapY - 1.3f);
 					}
+					//nakon toga svaki deo normalno radi sa snapY
 					else {
 						yCloseEnough = (uY <= snapY);
 
 					}
 
-					// Horizontal proximity
+					//OVO GORE SE DESAVA JER BOTTOM BUN NEMA RECT VERTICES, ALI MI SE NIJE DALO MENJATI
+
+					// Da li je X dovoljno blizu
 					bool xCloseEnough = fabs(uX - prev.x) <= 0.2f;
 
+					//AKO SE ISPUNJAVAJU OBA USLOVA ONDA MOZEMO STAVITI
 					if (xCloseEnough && yCloseEnough)
 					{
 						canPlace = true;
-						newX = prev.x;     // must align
+						newX = prev.x;    //ALIGNUJEMO TRENUTNO X I PROSLO X
+
+						//isto kao malopre za burger moramo umanjiti za 1.3
 						if (placedCount == 1) {
-							newY = prev.y - 1.3f;      // perfect stacking height
+							newY = prev.y - 1.3f;
 						}
+						//za ostale iteme sve okej i ne mora se adjustovatiu
 						else {
 							newY = prev.y + 0.05f;
 						}
-
-						std::cout << "Item"<<currentItem<<":" << newX << std::endl;
-						std::cout << "Item"<<currentItem<<":" << newY << std::endl;
 					}
 				}
 
+				//na kraju ako je canPlace true onda stavljamo odredjeni item
 				if (canPlace)
 				{
+
+					//texture,mesh,newX,newY je sve dobijeno kroz ovaj proces gore
 					placed[placedCount] = {
 						texture,
 						mesh,
 						newX,
 						newY,
-						(currentItem == 1) // cooked patties only
+						(currentItem == 1) // peceni burger 
 					};
 
+					//povecavamo placedCount i currentItem
 					placedCount++;
 					currentItem++;
 
+					//stavljamo offset na uY da bi se pojavilo na gornjem delu
 					if (currentItem < ORDER_COUNT)
 					{
 						uX = 0.0f;
@@ -629,20 +712,21 @@ int main()
 					}
 				}
 			}
+			//ako smo zavrsili iscrtavamo dodtano 
 			else {
 				greetings.Draw(shaderProgram, greetingsMesh.vao, 0.0f, 0.0f, 1.0f);
 			}
 			
 		}
 
-
+		//ako je tek pocetak onda se iscrtava samo dugme u sredini
 		else {
 			// texturing shader active
 			shaderProgram.Activate();
 			myButton.texture.Draw(shaderProgram, myButton.mesh.vao, 0.0f, 0.0f, 1.0f);
 		}
 
-
+		//za dugme->kada se klikne krece porudzbina
 		if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
 		{
 			double mouseX, mouseY;
@@ -667,11 +751,26 @@ int main()
 
 	// Delete window before ending the program
 	glfwDestroyWindow(window);
+
+	//brisemo sve teksture
 	smederevac.Delete();
 	patty.Delete();
 	bottomBun.Delete();
+	topBun.Delete();
 	table.Delete();
 	plate.Delete();
+	ketchup.Delete();
+	ketchupLayer.Delete();
+	ketchupSplash.Delete();
+	mustard.Delete();
+	mustardSplash.Delete();
+	mustardLayer.Delete();
+	onion.Delete();
+	pickles.Delete();
+	lettuce.Delete();
+	cheese.Delete();
+	greetings.Delete();
+
 	// Terminate GLFW before ending the program
 	glfwTerminate();
 	return 0;
